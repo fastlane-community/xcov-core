e5bf74744443ebbc6b73e572a7d2f74f9e57e9aa
Carlos Vidal <nakioparkour@gmail.com>
2016-01-24 19:18:55 +0000

    created project and added dependencies

---
+++ xCoverage-Core/xCoverage-Core/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  xCoverage-Core
+//
+//  Created by Carlos Vidal Pallin on 24/01/2016.
+//  Copyright © 2016 nakioStudio. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ xCoverage-Core/xCoverage-Core/AppDelegate.m
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.m
+//  xCoverage-Core
+//
+//  Created by Carlos Vidal Pallin on 24/01/2016.
+//  Copyright © 2016 nakioStudio. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+@end
