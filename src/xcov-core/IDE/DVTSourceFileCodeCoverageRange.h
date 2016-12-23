ece2780f15753a51769f53a42cd9121a16f398d4
Andrei Raifura <andrei.raifura@yopeso.com>
2016-12-23 17:54:47 +0200

    Remove wrongly computed 'executable' property

--- src/xcov-core/IDE/DVTSourceFileCodeCoverageRange.h
+++ src/xcov-core/IDE/DVTSourceFileCodeCoverageRange.h
@@ -7,7 +7,6 @@
 
 @interface DVTSourceFileCodeCoverageRange : NSObject <NSCoding>
 
-@property(readonly, nonatomic) BOOL executable;
 @property(readonly, nonatomic) unsigned long long length;
 @property(readonly, nonatomic) unsigned long long column;
 @property(readonly, nonatomic) unsigned long long line;
