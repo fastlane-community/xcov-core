8d264166e5e8faa6ad1273bc6487d952ce14d597
Connor Tumbleson <connor@sourcetoad.com>
2025-11-14 10:20:59 -0500

    build: update binary to v0.8

--- src/xcov-core/Middleware.m
+++ src/xcov-core/Middleware.m
@@ -7,7 +7,7 @@
 #import "DDCliApplication.h"
 #import "Core.h"
 
-NSString *const MiddlewareAppVersion    = @"0.7";
+NSString *const MiddlewareAppVersion    = @"0.8";
 NSString *const MiddlewareAppName       = @"xcov-core";
 
 @implementation Middleware
