5e28f42aeb18b97877c75b78bc85d0502922172f
Connor Tumbleson <connor.tumbleson@gmail.com>
2026-08-10 21:42:35 -0400

    build: move to version 1.0

--- src/xcov-core/Middleware.m
+++ src/xcov-core/Middleware.m
@@ -7,7 +7,7 @@
 #import "DDCliApplication.h"
 #import "Core.h"
 
-NSString *const MiddlewareAppVersion    = @"0.8";
+NSString *const MiddlewareAppVersion    = @"1.0";
 NSString *const MiddlewareAppName       = @"xcov-core";
 
 @implementation Middleware
